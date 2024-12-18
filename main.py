448a293843e4167e6f6e13ce68118b933c429d4c
bestzyq <21013965@mail.ecust.edu.cn>
2024-12-18 11:21:13 +0800

    fix bug

--- main.py
+++ main.py
@@ -860,7 +860,7 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
     rcon_port = 25575
 
     if not params:
-        await message.reply(content="请提供 Minecraft 服务器命令（say/list/tp/player/time/weather/kill/永昼机）")
+        await message.reply(content="请提供 Minecraft 服务器命令（say/list/永昼机）")
         return
     else:
         # 直接使用 params 作为 Minecraft 命令
@@ -878,8 +878,8 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
                 await message.reply(content=f"连接 Minecraft 服务器时发生错误: {str(e)}")
             return True
 
-        if mc_command not in {"list"} and not any(mc_command.startswith(prefix) for prefix in ["say", "tp", "player", "time", "weather", "kill"]):
-            await message.reply(content="请提供合法的 Minecraft 服务器命令（say/list/tp/player/time/weather/kill/永昼机）")
+        if mc_command not in {"list"} and not any(mc_command.startswith(prefix) for prefix in ["say", "tp"]):
+            await message.reply(content="请提供合法的 Minecraft 服务器命令（say/list/永昼机）")
         else:
             try:
                 # 连接到 RCON 服务器
