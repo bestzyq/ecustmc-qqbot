6562d41bd4af8bfb32c5af7600a62158e8ada790
bestzyq <21013965@mail.ecust.edu.cn>
2024-12-18 12:10:03 +0800

    add 关闭永昼机

--- main.py
+++ main.py
@@ -860,7 +860,7 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
     rcon_port = 25575
 
     if not params:
-        await message.reply(content="请提供 Minecraft 服务器命令（say/list/永昼机）")
+        await message.reply(content="请提供 Minecraft 服务器命令（say/list/永昼机/关闭永昼机）")
         return
     else:
         # 直接使用 params 作为 Minecraft 命令
@@ -878,8 +878,19 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
                 await message.reply(content=f"连接 Minecraft 服务器时发生错误: {str(e)}")
             return True
 
+        if mc_command == "关闭永昼机":
+            # 特殊处理命令为“永昼机”的情况
+            try:
+                with MCRcon(rcon_host, rcon_password, port=rcon_port) as mcr:
+                    # 执行多个命令
+                    mcr.command("player bot_sleep kill")
+                    await message.reply(content="永昼机已启动")
+            except Exception as e:
+                await message.reply(content=f"连接 Minecraft 服务器时发生错误: {str(e)}")
+            return True
+
         if mc_command not in {"list"} and not any(mc_command.startswith(prefix) for prefix in ["say", "tp"]):
-            await message.reply(content="请提供合法的 Minecraft 服务器命令（say/list/永昼机）")
+            await message.reply(content="请提供合法的 Minecraft 服务器命令（say/list/永昼机/关闭永昼机）")
         else:
             try:
                 # 连接到 RCON 服务器
