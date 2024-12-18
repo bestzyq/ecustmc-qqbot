ed3247c8370cee9cf9aeaf839161e704aae01f3e
bestzyq <21013965@mail.ecust.edu.cn>
2024-12-18 16:10:30 +0800

    add 刷铁机

--- main.py
+++ main.py
@@ -860,7 +860,7 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
     rcon_port = 25575
 
     if not params:
-        await message.reply(content="请提供 Minecraft 服务器命令（say/list/永昼机/关闭永昼机）")
+        await message.reply(content="请提供 Minecraft 服务器命令（say/list/永昼机/关闭永昼机/刷铁机/关闭刷铁机）")
         return
     else:
         # 直接使用 params 作为 Minecraft 命令
@@ -879,7 +879,7 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
             return True
 
         if mc_command == "关闭永昼机":
-            # 特殊处理命令为“永昼机”的情况
+            # 特殊处理命令为“关闭永昼机”的情况
             try:
                 with MCRcon(rcon_host, rcon_password, port=rcon_port) as mcr:
                     # 执行多个命令
@@ -889,8 +889,30 @@ async def query_mc_command(api: BotAPI, message: GroupMessage, params=None):
                 await message.reply(content=f"连接 Minecraft 服务器时发生错误: {str(e)}")
             return True
 
+        if mc_command == "刷铁机":
+            # 特殊处理命令为“刷铁机”的情况
+            try:
+                with MCRcon(rcon_host, rcon_password, port=rcon_port) as mcr:
+                    # 执行多个命令
+                    mcr.command("player bot_iron spawn at -3215 60 9375")
+                    await message.reply(content="刷铁机已启动")
+            except Exception as e:
+                await message.reply(content=f"连接 Minecraft 服务器时发生错误: {str(e)}")
+            return True
+
+        if mc_command == "关闭刷铁机":
+            # 特殊处理命令为“关闭刷铁机”的情况
+            try:
+                with MCRcon(rcon_host, rcon_password, port=rcon_port) as mcr:
+                    # 执行多个命令
+                    mcr.command("player bot_iron kill")
+                    await message.reply(content="刷铁机已关闭")
+            except Exception as e:
+                await message.reply(content=f"连接 Minecraft 服务器时发生错误: {str(e)}")
+            return True
+
         if mc_command not in {"list"} and not any(mc_command.startswith(prefix) for prefix in ["say", "tp"]):
-            await message.reply(content="请提供合法的 Minecraft 服务器命令（say/list/永昼机/关闭永昼机）")
+            await message.reply(content="请提供合法的 Minecraft 服务器命令（say/list/永昼机/关闭永昼机/刷铁机/关闭刷铁机）")
         else:
             try:
                 # 连接到 RCON 服务器
